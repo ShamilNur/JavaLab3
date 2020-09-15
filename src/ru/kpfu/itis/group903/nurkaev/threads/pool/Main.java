5032a0d7b68dd2dd2591cf1a5caecd097715f37d
ShamilNurkaev <62756126+ShamilNur@users.noreply.github.com>
2020-09-15 10:02:29 +0300

    implemented a ThreadPool

---
+++ src/ru/kpfu/itis/group903/nurkaev/threads/pool/Main.java
@@ -0,0 +1,47 @@
+/**
+ * @nshamil Shamil Nurkaev
+ * 11-903
+ * Homework
+ */
+
+package ru.kpfu.itis.group903.nurkaev.threads.pool;
+
+public class Main {
+    public static void main(String[] args) {
+        Runnable task1 = newTask('A');
+        Runnable task2 = newTask('B');
+        Runnable task3 = newTask('C');
+        Runnable task4 = newTask('D');
+
+//        // 1) Одна задача - один поток
+//        ThreadPool threadPool1 = new ThreadPool(1);
+//        threadPool1.submit(task1);
+
+//        // 2) Две задачи - один поток (поочередно выполнить каждую)
+//        ThreadPool threadPool2 = new ThreadPool(1);
+//        threadPool2.submit(task1);
+//        threadPool2.submit(task2);
+
+//        // 3) Три задачи - три потока (каждый поток выполняет свою задачу)
+//        ThreadPool threadPool3 = new ThreadPool(3);
+//        threadPool3.submit(task1);
+//        threadPool3.submit(task2);
+//        threadPool3.submit(task3);
+//
+        // 4) Четыре задачи - три потока (три потока выполняют три задачи, четвертая задача выполняется первым свободным)
+        ThreadPool threadPool4 = new ThreadPool(3);
+        threadPool4.submit(task1);
+        threadPool4.submit(task2);
+        threadPool4.submit(task3);
+        threadPool4.submit(task4);
+    }
+
+    private static Runnable newTask(char c) {
+        return () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + ' ' + c);
+            }
+        };
+    }
+}
+
---
+++ src/ru/kpfu/itis/group903/nurkaev/threads/pool/ThreadPool.java
@@ -0,0 +1,56 @@
+/**
+ * @nshamil Shamil Nurkaev
+ * 11-903
+ * Homework
+ */
+
+package ru.kpfu.itis.group903.nurkaev.threads.pool;
+
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+// wait, notify, synchronized
+public class ThreadPool {
+    // очередь задач
+    private final Deque<Runnable> tasks;
+
+    public ThreadPool(int threadsCount) {
+        tasks = new ConcurrentLinkedDeque<>();
+        // пул потоков
+        PoolWorker[] threads = new PoolWorker[threadsCount];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new PoolWorker();
+            threads[i].start();
+        }
+    }
+
+
+    public void submit(Runnable task) {
+        synchronized (tasks) {
+            tasks.push(task);
+            tasks.notify();
+        }
+    }
+
+    // класс - рабочий поток
+    private class PoolWorker extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            throw new IllegalStateException(e);
+                        }
+                    }
+                }
+
+                Runnable currentTask = tasks.removeLast();
+                currentTask.run();
+            }
+        }
+    }
+}
